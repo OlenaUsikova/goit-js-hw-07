949e273b11adb0494dbdb54bd5b9b2061646b2d3
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-15 15:26:57 +0100

    Tasks 1-9

---
+++ js/01-gallery.js
@@ -0,0 +1,10 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+galleryItems.forEach((item) => {
+const divEl = gallery.createElement('div');
+divEl})
