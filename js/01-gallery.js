5d01139780dcebf4ccd7600dcaaa8c3795dd835b
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-15 18:12:43 +0100

    Task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,35 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery")
 
+let backup = ``;
 galleryItems.forEach((item) => {
-const divEl = gallery.createElement('div');
-divEl})
+    backup += `<div class="gallery__item">
+    <a class="gallery__link" href=${item.original}>
+      <img
+        class="gallery__image"
+        src=${item.preview}
+        data-source=${item.original}
+        alt=${item.description}
+      />
+    </a>
+  </div>`;
+    });
+
+    gallery.insertAdjacentHTML("afterbegin", backup);
+
+//    const itemLink = gallery.querySelector(".gallery__link");
+//    itemLink.addEventListener("click", (ev) => {
+//     ev.preventDefault();
+//    })
+
+   gallery.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    if (ev.target.dataset.source) {
+        const instance = basicLightbox.create(`
+        <img src=${ev.target.dataset.source} width="1200">
+    `)
+        instance.show()
+    }
+        
+   })
+
